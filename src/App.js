dadfa8af6d803fc780eb6e8a2684aa670a0c130e
BG <bassem.ghoniem@yahoo.com>
2019-06-15 19:34:34 +0200

    first deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dockerized React :)
+          My first deploy :D
         </p>
         <a
           className="App-link"
